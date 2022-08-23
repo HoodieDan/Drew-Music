621d3f4c45986e0d049283722c8367c58191623d
HoodieDan <obodedaniel3@gmail.com>
2022-08-23 07:13:45 +0100

    improved upload experience

--- music/src/components/Upload.vue
+++ music/src/components/Upload.vue
@@ -1,60 +1,53 @@
 <template>
   <div class="bg-dark text-white rounded border border-gray-200 relative flex flex-col">
     <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-    <span class="card-title">Upload</span>
-    <i class="fas fa-upload float-right text-green-400 text-2xl"></i>
+        <span class="card-title">Upload</span>
+        <i class="fas fa-upload float-right text-green-400 text-2xl"></i>
     </div>
     <div class="p-6">
-    <!-- Upload Dropbox -->
-    <div
-        class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed
-        border-gray-400 text-gray-400 transition duration-500 hover:text-white
-        hover:bg-green-400 hover:border-green-400 hover:border-solid"
-        :class="{ 'bg-green-400 border-green-400 border-solid': isDraggedOver }"
-        @drag.prevent.stop=""
-        @dragstart.prevent.stop=""
-        @dragend.prevent.stop="isDraggedOver = false"
-        @dragover.prevent.stop="isDraggedOver = true"
-        @dragenter.prevent.stop="isDraggedOver = true"
-        @dragleave.prevent.stop="isDraggedOver = false"
-        @drop.prevent.stop="upload($event)">
-        <h5>Drop your files here</h5>
-    </div>
-    <hr class="my-6" />
-    <!-- Progess Bars -->
-    <div class="mb-4">
-        <!-- File Name -->
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-        <!-- Inner Progress Bar -->
-        <div class="transition-all progress-bar bg-blue-400" style="width: 75%"></div>
+        <!-- Upload Dropbox -->
+        <div
+            class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed
+            border-gray-400 text-gray-400 transition duration-500 hover:text-white
+            hover:bg-green-400 hover:border-green-400 hover:border-solid"
+            :class="{ 'bg-green-400 border-green-400 border-solid': isDraggedOver }"
+            @drag.prevent.stop=""
+            @dragstart.prevent.stop=""
+            @dragend.prevent.stop="isDraggedOver = false"
+            @dragover.prevent.stop="isDraggedOver = true"
+            @dragenter.prevent.stop="isDraggedOver = true"
+            @dragleave.prevent.stop="isDraggedOver = false"
+            @drop.prevent.stop="upload($event)">
+            <h5>Drop your files here</h5>
         </div>
-    </div>
-    <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-        <div class="transition-all progress-bar bg-blue-400" style="width: 35%"></div>
+        <hr class="my-6" />
+        <!-- Progess Bars -->
+        <div class="mb-4" v-for="upload in uploads" :key="upload.name">
+            <!-- File Name -->
+            <div class="font-bold text-sm" :class="upload.text_class">
+                <i :class="upload.icon"></i> {{ upload.name }}
+            </div>
+            <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
+            <!-- Inner Progress Bar -->
+            <div class="transition-all progress-bar bg-blue-400" 
+                :style="{ width: upload.current_progress + '%' }"
+                :class="upload.variant"></div>
+            </div>
         </div>
-    </div>
-    <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-        <div class="transition-all progress-bar bg-blue-400" style="width: 55%"></div>
-        </div>
-    </div>
     </div>
   </div>
 </template>
 
 <script>
 import { storage } from '@/includes/firebase'
-import { ref, uploadBytes } from 'firebase/storage'
+import { ref, uploadBytesResumable } from 'firebase/storage'
 
 export default {
     name: 'Upload',
     data () {
         return {
             isDraggedOver: false,
+            uploads: [],
         }
     },
     methods: {
@@ -69,11 +62,20 @@ export default {
                 }
                 console.log(file)
                 const storageRef = ref(storage, `songs/${file.name}`)
-                // const songsRef = storageRef.child(`songs/${file.name}`)
-                // songsRef.put(file)
-                uploadBytes(storageRef, file) .then((snapshot) => {
-                    console.log(snapshot)
-                })
+                
+                const task = uploadBytesResumable(storageRef, file)
+                const uploadIndex = this.uploads.push({
+                    task,
+                    current_progress: 0,
+                    name: file.name,
+                    variant: 'bg-blue-400',
+                    icon: 'fas fa-spinner fa-spin',
+                    text_class: ''
+                }) - 1;
+                task.on('state_changed', (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    this.uploads[uploadIndex].current_progress = progress;
+                });
             })
         }
     }
