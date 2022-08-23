fa337fa013e09688d20a82e944729d68f875d8f3
HoodieDan <obodedaniel3@gmail.com>
2022-08-23 01:46:40 +0100

    checked to make sure file type is audio

--- music/src/components/Upload.vue
+++ music/src/components/Upload.vue
@@ -17,7 +17,7 @@
         @dragover.prevent.stop="isDraggedOver = true"
         @dragenter.prevent.stop="isDraggedOver = true"
         @dragleave.prevent.stop="isDraggedOver = false"
-        @drop.prevent.stop="upload">
+        @drop.prevent.stop="upload($event)">
         <h5>Drop your files here</h5>
     </div>
     <hr class="my-6" />
@@ -55,9 +55,17 @@ export default {
         }
     },
     methods: {
-        upload() {
+        upload($event) {
             this.isDraggedOver = false;
-            
+
+            const files = [...$event.dataTransfer.files]
+
+            files.forEach((file) => {
+                if (file.type !== 'audio/mpeg') {
+                    return;
+                }
+            })
+            console.log($event)
         }
     }
 }
